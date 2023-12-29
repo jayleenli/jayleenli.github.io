79f04ac8ceac7f2a0ec94685ac18155dfc7c4596
Jayleen Li <jayleenli17@gmail.com>
2023-12-29 14:13:22 -0800

    Retrieve JSON from endpoint to locally, add JSON loading for publications, fix date sorting bug on art page, make project showcase css a bit better

--- js/index.js
+++ js/index.js
@@ -84,60 +84,52 @@ function createColumns(project_items_list) {
 	var divided_project_section = [];
 	const group_count = Math.ceil(project_items_list.length/3.0);
 	
-	divided_project_section.push(`<div class='col-md-4'>`);
-	divided_project_section.push(project_items_list.slice(0, group_count));
+	divided_project_section.push(`<div class='col-md-4 animate-box' data-animate-effect='fadeInLeft'>`);
+	divided_project_section.push(project_items_list.slice(0, group_count).join(""));
 	divided_project_section.push(`</div>`);
-	divided_project_section.push(`<div class='col-md-4'>`);
-	divided_project_section.push(project_items_list.slice(group_count, group_count*2));
+	divided_project_section.push(`<div class='col-md-4 animate-box' data-animate-effect='fadeInLeft'>`);
+	divided_project_section.push(project_items_list.slice(group_count, group_count*2).join(""));
 	divided_project_section.push(`</div>`);
-	divided_project_section.push(`<div class='col-md-4'>`);
-	divided_project_section.push(project_items_list.slice(group_count*2, project_items_list.length));
+	divided_project_section.push(`<div class='col-md-4 animate-box' data-animate-effect='fadeInLeft'>`);
+	divided_project_section.push(project_items_list.slice(group_count*2, project_items_list.length).join(""));
 	divided_project_section.push(`</div>`);
 
 	return divided_project_section.join("");
 }
 
-/* Test data locally. God Bless for ChatGPT converting all my shit into JSON for me ~chefs kiss~ */ 
-// var json_test = [insert data here];
+/* God Bless for ChatGPT converting all my shit into JSON for me ~chefs kiss~ */ 
 
-// Insert the endpoint code here
+var featured_items = [];
+var hackathon_items = [];
+var leadership_involvement_items = [];
+var personal_items = [];
 
-// /* Test data locally end. */ 
+project_showcase_json.forEach(function(item) {
+	var item_html = [];
+	item_html.push(`<div class='services ${determineColor(item["category"])}'><div class='blog-entry'>`)
+	item_html.push(createImageHeader(item["links"]["href"], item["img_src"], item["img_alt_text"]));
+	item_html.push(`<div class="desc">`);
+	item_html.push(createTitle(item["links"]["href"], item["project_name"], item["subheader"]));
+	item_html.push(`<p>${item["description_html"]}</p>`);
+	item_html.push(`<span>`);
+	item_html.push(createLinks(item["links"]));
+	item_html.push(createDateFooter(item["date"]));
+	item_html.push(`</span>`);
+	item_html.push("</div></div></div>");
 
-// Real data hitting endpoint
-$.getJSON("https://jayleenli.github.io/data/project_showcase.json", function( data ) {
-	var featured_items = [];
-	var hackathon_items = [];
-	var leadership_involvement_items = [];
-	var personal_items = [];
-
-	data.forEach(function(item) {
-		var item_html = [];
-		item_html.push(`<div class='services ${determineColor(item["category"])}'><div class='blog-entry'>`)
-		item_html.push(createImageHeader(item["links"]["href"], item["img_src"], item["img_alt_text"]));
-		item_html.push(`<div class="desc">`);
-		item_html.push(createTitle(item["links"]["href"], item["project_name"], item["subheader"]));
-		item_html.push(`<p>${item["description_html"]}</p>`);
-		item_html.push(`<span>`);
-		item_html.push(createLinks(item["links"]));
-		item_html.push(createDateFooter(item["date"]));
-		item_html.push(`</span>`);
-		item_html.push("</div></div></div>");
-
-		if (item["featured"] === true) {
-			featured_items.push(item_html.join(""));
-		}
-		switch (item["category"]) {
-			case "hackathon": hackathon_items.push(item_html.join("")); break;
-			case "leadership_involvement": leadership_involvement_items.push(item_html.join("")); break;
-			case "personal": personal_items.push(item_html.join("")); break;
-			default:
-				console.error("unhandled type");
-		}
-	});
-
-	$( "#showcase-featured" ).append(createColumns(featured_items));
-	$( "#showcase-hackathons" ).append(createColumns(hackathon_items));
-	$( "#showcase-leadership" ).append(createColumns(leadership_involvement_items));
-	$( "#showcase-personal" ).append(createColumns(personal_items));
+	if (item["featured"] === true) {
+		featured_items.push(item_html.join(""));
+	}
+	switch (item["category"]) {
+		case "hackathon": hackathon_items.push(item_html.join("")); break;
+		case "leadership_involvement": leadership_involvement_items.push(item_html.join("")); break;
+		case "personal": personal_items.push(item_html.join("")); break;
+		default:
+			console.error("unhandled type");
+	}
 });
+
+$( "#showcase-featured" ).append(createColumns(featured_items));
+$( "#showcase-hackathons" ).append(createColumns(hackathon_items));
+$( "#showcase-leadership" ).append(createColumns(leadership_involvement_items));
+$( "#showcase-personal" ).append(createColumns(personal_items));
